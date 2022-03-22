94b8da959622c3a490345addd472f89f1240c0b9
evelynhelena <evelynhelena781@gmail.com>
2022-03-22 20:10:32 -0300

    criando dashboard e pagina de usuário

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Input, Text, Icon} from "@chakra-ui/react";
-import {RiSearchLine} from 'react-icons/ri'
+import { Flex, Input, Text, Icon, HStack, Box, Avatar } from "@chakra-ui/react";
+import { RiNotificationLine, RiSearchLine, RiUserAddLine, } from 'react-icons/ri'
 
 export function Header() {
     return (
@@ -35,14 +35,43 @@ export function Header() {
                 bg="gray.800"
                 borderRadius="full">
                 <Input
-                  color="gray.50"
-                  variant="unstyled"
-                  placeholder="Buscar na Plataforma"
-                  _placeholder={{color:
-                  'gray.400'}}
-                  px="4"
-                  mr="4"/>
-                  <Icon as={RiSearchLine} fontSize="20"/>
+                    color="gray.50"
+                    variant="unstyled"
+                    placeholder="Buscar na Plataforma"
+                    _placeholder={{
+                        color:
+                            'gray.400'
+                    }}
+                    px="4"
+                    mr="4" />
+                <Icon as={RiSearchLine} fontSize="20" />
+            </Flex>
+
+            <Flex
+                align="center"
+                ml="auto"
+            >
+                <HStack
+                  spacing="8"
+                  mx="8"
+                  pr="8"
+                  py="1"
+                  color="gray.300"
+                  borderRightWidth={1}
+                  borderColor="gray.700"
+                >
+                    <Icon as={RiNotificationLine} fontSize="20" />
+                    <Icon as={RiUserAddLine} fontSize="20" />
+                </HStack>
+
+
+                <Flex align="center">
+                    <Box mr="4" textAlign="right">
+                        <Text>Evelyn Helena</Text>
+                        <Text color="gray.300" fontSize="small">evelyn.helena1@gmail.com</Text>
+                    </Box>
+                    <Avatar size="md" name="Evelyn Helena" src="https://github.com/evelynhelena.png" />
+                </Flex>
             </Flex>
         </Flex>
     )
