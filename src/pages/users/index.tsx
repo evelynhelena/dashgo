42ea92d43b491dc923a0d46881baa6a76d04c705
evelynhelena <evelynhelena781@gmail.com>
2022-04-27 15:27:19 -0300

    Implementando paginação (em construção)

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,23 +1,20 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
+import { Box, Button, Checkbox, Flex, Heading, Icon, Spinner, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
 import Link from "next/link";
-import { useEffect } from "react";
-import { RiAddLine} from "react-icons/ri";
-import {useQuery} from 'react-query';
+import { RiAddLine,RiRefreshLine } from "react-icons/ri";
+import { useQuery } from 'react-query';
 
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
+import { api } from "../../services/api";
+import { useUsers } from "../../services/hooks/useUsers";
 
 export default function UserList() {
+    const { data, isLoading, isFetching, error, refetch } = useUsers();
+
     const isWideVersion = useBreakpointValue({
         base: false,
         lg: true,
-    })
-
-    useEffect(() => {
-        fetch('http://localhost:3000/api/users')
-        .then(response => response.json())
-        .then(data => console.log(data))
     })
 
     return (
@@ -34,89 +31,80 @@ export default function UserList() {
 
                 <Box flex="1" borderRadius={8} bg="gray.800" p="8">
                     <Flex mb="8" justify="space-between" align="center">
-                        <Heading size="lg" fontWeight="normal">Usuários</Heading>
-                        <Link href="/users/create" passHref>
+                        <Heading size="lg" fontWeight="normal">
+                            Usuários
+
+                            {!isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4" />}
+                        </Heading>
+                        <Flex justify="center">
                             <Button
-                                as="a"
                                 size="sm"
-                                fontSize="sm"
                                 colorScheme="pink"
-                                leftIcon={<Icon
-                                    as={RiAddLine} fontSize="20" />
-                                }
                                 cursor="pointer"
+                                onClick={() => refetch()}
+                                mr="3"
                             >
-                                Criar Novo
+                                <Icon as={RiRefreshLine} fontSize="20" />
                             </Button>
-                        </Link>
+                            <Link href="/users/create" passHref>
+                                <Button
+                                    as="a"
+                                    size="sm"
+                                    fontSize="sm"
+                                    colorScheme="pink"
+                                    leftIcon={<Icon
+                                        as={RiAddLine} fontSize="20" />
+                                    }
+                                    cursor="pointer"
+                                >
+                                    Criar Novo
+                                </Button>
+                            </Link>
+                        </Flex>
 
                     </Flex>
+                    {isLoading ? (
+                        <Flex justify="center">
+                            <Spinner />
+                        </Flex>
+                    ) : error ? (
+                        <Flex justify="center">
+                            <Text>Falha ao obter dados dos usuários</Text>
+                        </Flex>
+                    ) : (
+                        <>
+                            <Table colorScheme="whiteAlpha">
+                                <Thead>
+                                    <Tr>
+                                        <Th px={{ sm: "4", md: "4", lg: "6" }} color="gray.300" width="8">
+                                            <Checkbox colorScheme="pink" />
+                                        </Th>
+                                        <Th>Usuário</Th>
+                                        {isWideVersion && <Th>Data de Cadastro</Th>}
+                                    </Tr>
+                                </Thead>
+                                <Tbody>
+                                    {data.map(user => {
+                                        return (
+                                            <Tr key={user.id}>
+                                                <Td px={{ sm: "4", md: "4", lg: "6" }}>
+                                                    <Checkbox colorScheme="pink" />
+                                                </Td>
+                                                <Td>
+                                                    <Box>
+                                                        <Text fontWeight="bold">{user.name}</Text>
+                                                        <Text fontSize="sm" color="gray.300">{user.email}</Text>
+                                                    </Box>
+                                                </Td>
+                                                {isWideVersion && <Td>{user.createdAt}</Td>}
 
-                    <Table colorScheme="whiteAlpha">
-                        <Thead>
-                            <Tr>
-                                <Th px={{ sm: "4", md: "4", lg: "6" }} color="gray.300" width="8">
-                                    <Checkbox colorScheme="pink" />
-                                </Th>
-                                <Th>Usuário</Th>
-                                {isWideVersion && <Th>Data de Cadastro</Th>}
-                            </Tr>
-                        </Thead>
-                        <Tbody>
-                            <Tr>
-                                <Td px={{ sm: "4", md: "4", lg: "6" }}>
-                                    <Checkbox colorScheme="pink" />
-                                </Td>
-                                <Td>
-                                    <Box>
-                                        <Text fontWeight="bold">Evelyn Helena</Text>
-                                        <Text fontSize="sm" color="gray.300">evelyn.helena1@gmail.com</Text>
-                                    </Box>
-                                </Td>
-                                {isWideVersion && <Td>04 de Abril,2022</Td>}
-
-                            </Tr>
-                            <Tr>
-                                <Td px={{ sm: "4", md: "4", lg: "6" }}>
-                                    <Checkbox colorScheme="pink" />
-                                </Td>
-                                <Td>
-                                    <Box>
-                                        <Text fontWeight="bold">Evelyn Helena</Text>
-                                        <Text fontSize="sm" color="gray.300">evelyn.helena1@gmail.com</Text>
-                                    </Box>
-                                </Td>
-                                {isWideVersion && <Td>04 de Abril,2022</Td>}
-
-                            </Tr>
-                            <Tr>
-                                <Td px={{ sm: "4", md: "4", lg: "6" }}>
-                                    <Checkbox colorScheme="pink" />
-                                </Td>
-                                <Td>
-                                    <Box>
-                                        <Text fontWeight="bold">Evelyn Helena</Text>
-                                        <Text fontSize="sm" color="gray.300">evelyn.helena1@gmail.com</Text>
-                                    </Box>
-                                </Td>
-                                {isWideVersion && <Td>04 de Abril,2022</Td>}
-
-                            </Tr>
-                            <Tr>
-                                <Td px={{ sm: "4", md: "4", lg: "6" }}>
-                                    <Checkbox colorScheme="pink" />
-                                </Td>
-                                <Td>
-                                    <Box>
-                                        <Text fontWeight="bold">Evelyn Helena</Text>
-                                        <Text fontSize="sm" color="gray.300">evelyn.helena1@gmail.com</Text>
-                                    </Box>
-                                </Td>
-                                {isWideVersion && <Td>04 de Abril,2022</Td>}
-
-                            </Tr>
-                        </Tbody>
-                    </Table>
+                                            </Tr>
+                                        )
+                                    })}
+                                </Tbody>
+                            </Table>
+                        </>
+                    )}
                     <Pagination />
                 </Box>
             </Flex>
